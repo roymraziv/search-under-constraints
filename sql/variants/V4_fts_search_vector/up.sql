2f57b892eb51893887e627291c371d9acc02d495
Roy <roywmraziv@icloud.com>
2026-02-05 13:21:59 -0600

    add up sql

--- sql/variants/V4_fts_search_vector/up.sql
+++ sql/variants/V4_fts_search_vector/up.sql
@@ -1,9 +1,5 @@
 BEGIN;
 
-ALTER TABLE products ADD COLUMN search_vector tsvector
-GENERATED ALWAYS AS (
-    to_tsvector('english', concat_ws(' ', name, brand, category, description))
-) STORED;
 
 CREATE INDEX IF NOT EXISTS idx_products_search_vector ON products USING gin (search_vector);
 
